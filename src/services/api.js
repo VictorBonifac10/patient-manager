c70da21eaa5886a9f6e054bdbd6a9fd779012401
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-09-04 16:16:46 -0300

    Criacao da pasta dist p/ hospedagem

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://api-cadastro-usuarios-77pd.onrender.com/'
+  baseURL: 'https://api-cadastro-usuarios-77pd.onrender.com/'
 })
 
 export default api
